19c4db58bfe227940048ee23fef4976c700e2bdc
Steve McGee <steve@crmscience.com>
2023-03-04 23:50:57 -0500

    Drink app v3

--- DrinksApp.py
+++ DrinksApp.py
@@ -1,48 +1,62 @@
 import json
-from tkinter import *
-from tkinter import ttk
 import requests
-
-root = Tk()
-root.title('Drink DB')
-root.geometry('1200x400')
-
-# Create a main frame
-frame = LabelFrame(root,padx=250,pady=10)
-frame.pack(side=BOTTOM,fill=BOTH, expand=1)
-
-randomBtn = Button(frame, text='Random Drink',command=lambda: randomClick()).grid(row=0, column =0)
-nameBtn = Button(frame, text='Drink by Name',command=lambda: nameClick()).grid(row=0, column =1)
-letterBtn = Button(frame, text='Drink by First Letter',command=lambda: call_drink()).grid(row=0, column =2)
-ingredientBtn = Button(frame, text='Drink by Ingredient',command=lambda: api_drink_first_letter()).grid(row=0, column =3)
+import tkinter as tk
+from tkinter import ttk
 
 
 
-# Create a canvas
-app_canvas = Canvas(frame)
-app_canvas.grid(row=2, column =0, columnspan=4)
+class ScrollableFrame(tk.Frame):
+    def __init__(self, container, *args, **kwargs):
+        super().__init__(container, *args, **kwargs)
 
-# Add a Scroll bar
-v_scroll=ttk.Scrollbar(frame, orient=VERTICAL, command=app_canvas.yview)
-v_scroll.grid(row=2, column =4)
+        self.canvas = tk.Canvas(self)
+        self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame = tk.Frame(self.canvas)
 
-# configure the canvas
-app_canvas.configure(yscrollcommand=v_scroll.set)
-app_canvas.bind('<Configure>', lambda e: app_canvas.configure(scrollregion=app_canvas.bbox('all')))
+        self.scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
 
-# create ANOTHER frame inside the Canvas
-inner_frame = Frame(app_canvas)
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
 
-# add new frame to the canvas 
-app_canvas.create_window((0,0), window=inner_frame, anchor='nw')
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
 
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
 
-#v = Scrollbar(root, orient=VERTICAL)
-#v.grid(row=3,column=2,sticky='ns')
+def on_button_click(button):
+    varinput=tk.StringVar()
+    print(f"You clicked {button['text']} button")
+    if button == button1:
+        randomClick()
+    elif button == button2:
+        button5 = tk.Button(row2_frame, text=button['text'], command=lambda: call_drink(varinput.get()))
+        entry = tk.Entry(row2_frame,textvariable=varinput)
 
+        button5.pack(side="left")
+        entry.pack(side="left", fill="x", expand=True)
+    
+    elif button == button3:
+        pass
+        '''
+        button5 = tk.Button(row2_frame, text={button['text']},padx=100, command=lambda: call_drink(varinput.get()))
+        entry = tk.Entry(row2_frame)
 
-varinput=StringVar()
+        button5.pack(side="left")
+        entry.pack(side="left", fill="x", expand=True)
+        '''
+    else:
+        pass
+        '''
+        button5 = tk.Button(row2_frame, text={button['text']},padx=100, command=lambda: call_drink(varinput.get()))
+        entry = tk.Entry(row2_frame)
 
+        button5.pack(side="left")
+        entry.pack(side="left", fill="x", expand=True)
+        '''
+        
+        
+
+        
+    
 def api_random():
     response = requests.get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     data = response.json()
@@ -81,31 +95,23 @@ def processIngredient(data):
         api_drink_by_id(i['idDrink'])
 
 def randomClick():
-    print('Was Clicked')
     data = api_random()
     clear_frame()
     displayDrink(data)
     
-def nameClick():
-    #varinput.get()
-    nameLabel1=Label(frame, text="Drink Name",justify=LEFT).grid(row=1,column=0,sticky=W)
-    nameInput=Entry(frame,textvariable=varinput,justify=LEFT,width=47).grid(row=1,column=1,columnspan=4,sticky=W)
-    buttonDrink = Button(frame, text="Get My Drink", command=lambda: call_drink(varinput.get())).grid(row=1, column=4,sticky=W)
     
 def call_drink(name):
-    print(name)
+    #print(name)
     data = api_drink_name(name)
-    print(data)
+    #print(data)
     clear_frame()
     displayDrink(data)
     
 def clear_frame():
-    exists=inner_frame.winfo_exists()
-    if exists == 1:
-        for widget in inner_frame.winfo_children():
-            widget.destroy()
-        
-    
+    for widget in inner_frame.winfo_children():
+        widget.destroy()
+    for widget in row2_frame.winfo_children():
+        widget.destroy()
 
 def displayDrink(data):
     x=1 
@@ -114,61 +120,77 @@ def displayDrink(data):
     for i in data['drinks']:
         
         #print("Id:", i['idDrink'])
-        nameLabel=Label(inner_frame,text="Name:",justify=LEFT).grid(row=rowNum,column=0,sticky=W)            
-        name2Label=Label(inner_frame,text=i['strDrink'],justify=LEFT).grid(row=rowNum,column=1,sticky=W)
+        name=tk.Label(inner_frame,text="Name:").grid(row=rowNum,column=0,)            
+        name=tk.Label(inner_frame,text=i['strDrink']).grid(row=rowNum,column=1,)
         rowNum+=1
-        categoryLabel=Label(inner_frame,text="Category:",justify=LEFT).grid(row=rowNum,column=0,sticky=W)
-        category2Label=Label(inner_frame,text=i['strCategory'],justify=LEFT).grid(row=rowNum,column=1,sticky=W)
+        categoryLabel=tk.Label(inner_frame,text="Category:").grid(row=rowNum,column=0,)
+        category2Label=tk.Label(inner_frame,text=i['strCategory']).grid(row=rowNum,column=1,)
         rowNum+=1
-        alcoholicLabel=Label(inner_frame,text="Alcoholic:",justify=LEFT).grid(row=rowNum,column=0,sticky=W)
-        alcoholic2Label=Label(inner_frame,text=i['strAlcoholic'],justify=LEFT,padx=15).grid(row=3,column=1,sticky=W)
+        alcoholicLabel=tk.Label(inner_frame,text="Alcoholic:").grid(row=rowNum,column=0,)
+        alcoholic2Label=tk.Label(inner_frame,text=i['strAlcoholic'],padx=15).grid(row=3,column=1,)
         rowNum+=1
-        glassLabel=Label(inner_frame,text="Glass:",justify=LEFT).grid(row=rowNum,column=0,sticky=W)
-        glass2Label=Label(inner_frame,text=i['strGlass'],justify=LEFT).grid(row=rowNum,column=1,sticky=W)
+        glassLabel=tk.Label(inner_frame,text="Glass:").grid(row=rowNum,column=0,)
+        glass2Label=tk.Label(inner_frame,text=i['strGlass']).grid(row=rowNum,column=1,)
         rowNum+=1 
-        space1=Label(inner_frame,text="   ").grid(row=rowNum,column=0,sticky=W)
+        space1=tk.Label(inner_frame,text="   ").grid(row=rowNum,column=0,)
         rowNum+=1 
-        ingredientsLabel=Label(inner_frame,text='Ingredients:',justify=LEFT).grid(row=rowNum,column=0,sticky=W) 
+        ingredientsLabel=tk.Label(inner_frame,text='Ingredients:').grid(row=rowNum,column=0,) 
         rowNum+=1
         
         
         while x < 16: 
             if i['strIngredient'+str(x)] is not None:
-                ingredient=Label(inner_frame,text=i['strIngredient'+str(x)],justify=LEFT).grid(row=rowNum,column=0,sticky=W)
-                measure=Label(inner_frame,text=i['strMeasure'+str(x)],justify=LEFT).grid(row=rowNum,column=1,sticky=W)
+                ingredient=tk.Label(inner_frame,text=i['strIngredient'+str(x)]).grid(row=rowNum,column=0,)
+                measure=tk.Label(inner_frame,text=i['strMeasure'+str(x)]).grid(row=rowNum,column=1,)
                 rowNum+=1
             x=x+1
         
         if i['strInstructions'] is not None:
-            space2=Label(inner_frame,text="   ").grid(row=rowNum,column=0,sticky=W) 
+            space2=tk.Label(inner_frame,text="   ").grid(row=rowNum,column=0,) 
             rowNum+=1
-            measure=Label(inner_frame,text="Instructions:").grid(row=rowNum,column=0,sticky=W)
+            measure=tk.Label(inner_frame,text="Instructions:").grid(row=rowNum,column=0,)
             rowNum+=1
-            measure=Label(inner_frame,text= i['strInstructions'],justify=LEFT,wraplength=220).grid(row=rowNum,column=0,sticky=W)
+            measure=tk.Label(inner_frame,text= i['strInstructions'],wraplength=220).grid(row=rowNum,column=0,)
             
-        brLine=Label(inner_frame,text='   ').grid(row=x+11,column=0,sticky=W)
+        brLine=tk.Label(inner_frame,text='   ').grid(row=x+11,column=0,)
         rowNum+=2
-        
+
+root = tk.Tk()
+root.geometry("400x400")
+
+# Row 1
+row1_frame = tk.Frame(root)
+row1_frame.pack(fill="x")
+
+button1 = tk.Button(row1_frame, text="Random Drink", command=lambda: on_button_click(button1))
+button2 = tk.Button(row1_frame, text="Drink by Name", command=lambda: on_button_click(button2))
+button3 = tk.Button(row1_frame, text="Drink by First Letter", command=lambda: on_button_click(button3))
+button4 = tk.Button(row1_frame, text="Drink by Ingredient", command=lambda: on_button_click(button4))
+
+button1.pack(side="left")
+button2.pack(side="left")
+button3.pack(side="left")
+button4.pack(side="left")
+
+# Row 2
+row2_frame = tk.Frame(root)
+row2_frame.pack(fill="x")
+
+# Row 3
+row3_frame = tk.Frame(root)
+row3_frame.pack(fill="both", expand=True)
+
+scrollable_frame = ScrollableFrame(row3_frame)
+scrollable_frame.pack(side="top", fill="both", expand=True)
+
+inner_frame= tk.Frame(scrollable_frame.scrollable_frame)
+inner_frame.pack(expand=True)
 
 
+
+'''
+for i in range(50):
+    label = tk.Label(scrollable_frame.scrollable_frame, text=f"Label {i}")
+    label.pack()
+'''
 root.mainloop()
-
-'''
-choice = input('Would you like to Get a Random Drink (type \'r\')\nIf you want drinks by name (type \'n\')\nIf you want drinks by first letter of name (type \'l\')\nIf you want drinks by ingredient (type \'i\') : ')
-
-if choice == 'n':
-    name = input('Please input a drink name. : ')
-    data = api_drink_name(name)
-    displayDrink(data)
-elif choice == 'l':
-    letter = input('Please input the first letter of a drink. : ')
-    data = api_drink_first_letter(letter)
-    displayDrink(data)
-elif choice == 'i':
-    ingredient = input('Please input the drink ingredient. : ')
-    data = api_search_ingredient(ingredient)
-    processIngredient(data)
-else:
-    data = api_random()
-    displayDrink(data)
-'''
